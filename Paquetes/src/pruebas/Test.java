0de4b6cbb0053d427829e2b074ee7f64346a04d3
Nene Arce gonzalez <arcer@uabc.edu.mx>
2019-02-06 18:01:02 -0800

    Version1

---
+++ Paquetes/src/pruebas/Test.java
@@ -0,0 +1,14 @@
+package pruebas;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		paquete1.Persona p= new paquete1.Persona(23, "Maria");
+		paquete2.Persona m= new paquete2.Persona(24, "Pedro", "Maculino");
+	System.out.println(p.toString());
+	System.out.println(m.toString());
+	
+	}
+
+}
