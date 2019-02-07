48a1f6424a184391fe3d58d361e11deb9599abf2
Nene Arce gonzalez <arcer@uabc.edu.mx>
2019-02-06 18:26:12 -0800

    prueba

--- Paquetes/src/pruebas/Test.java
+++ Paquetes/src/pruebas/Test.java
@@ -5,13 +5,14 @@ public class Test {
 	public static void main(String[] args) {
 		
 		paquete1.Persona p= new paquete1.Persona(23, "Maria");
-		paquete2.Persona m= new paquete2.Persona(24, "Pedro", "Maculino");
+		paquete2.Persona m= new paquete2.Persona(23, "Ramon", "Maculino");
 		paquete1.Persona n= new paquete1.Persona(25, "Juan");
 		System.out.println("Inicio de impresion: ");
 		    System.out.println(p.toString());
 	        System.out.println(m.toString());
 	        System.out.println(n.toString());
 	        System.out.println("Fin..... ");
+	        System.out.println("prueba branch");
 	}
 
 }
