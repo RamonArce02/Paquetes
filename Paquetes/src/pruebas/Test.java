c3c93b986a7fa8009f9660029a2d2b08dbee03f9
Nene Arce gonzalez <arcer@uabc.edu.mx>
2019-02-06 18:21:38 -0800

    cambio2

--- Paquetes/src/pruebas/Test.java
+++ Paquetes/src/pruebas/Test.java
@@ -6,9 +6,11 @@ public class Test {
 		
 		paquete1.Persona p= new paquete1.Persona(23, "Maria");
 		paquete2.Persona m= new paquete2.Persona(24, "Pedro", "Maculino");
-	        System.out.println("Inicio de impresion: ");
-		System.out.println(p.toString());
+		paquete1.Persona n= new paquete1.Persona(25, "Juan");
+		System.out.println("Inicio de impresion: ");
+		    System.out.println(p.toString());
 	        System.out.println(m.toString());
+	        System.out.println(n.toString());
 	        System.out.println("Fin..... ");
 	}
 
